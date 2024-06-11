70a03621a7a9d16223b97be11f365b20e5ffce48
Efransc <116307362+Frunsc@users.noreply.github.com>
2024-06-12 01:05:33 +0300

    Add files via upload

---
+++ framework/views/21c2f09020526abf36ccae83494a7848896b5c06.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0.00
+    </div>
+    <strong>&copy; <?php echo e(date('Y')); ?> <a href="/"><?php echo e($setting->Efran_Sefa_Coskun); ?></a>.</strong> All rights
+    reserved.
+</footer><?php /**PATH C:\xampp\htdocs\ibp_project\PointofSale-Laravel\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
